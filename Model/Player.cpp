b23cac5250ebd5b25fb4d874a00b9026196a59fb
Amin_Seifi <aminseiifi@gmail.com>
2021-08-03 18:48:26 +0430

    Remove setScore, add playerId and complete constructor body

--- Model/Player.cpp
+++ Model/Player.cpp
@@ -1,5 +1,21 @@
-//
-// Created by amin on 8/3/21.
-//
-
 #include "Player.h"
+
+using namespace std;
+
+unsigned int Player::lastId = 0;
+
+Player::Player()
+{
+    id = ++lastId;
+    setName("Player " + to_string(getId()));
+    score = 0;
+    setIsAI(false);
+}
+
+Player::Player(string name, bool isAI)
+{
+    id = ++lastId;
+    setName(name);
+    score = 0;
+    setIsAI(isAI)
+}
--- Model/Player.h
+++ Model/Player.h
@@ -5,18 +5,21 @@
 class Player {
 public:
     Player();
-    explicit Player(std::string, unsigned int = 0, bool = false);
+    explicit Player(std::string, bool = false);
+    unsigned int getId() const;
     void setName(std::string);
     std::string getName() const;
-    void setScore(unsigned int);
+    void scoreIncrement();
     unsigned int getScore() const;
     void setIsAI(bool);
     bool getIsAI() const;
 
 private:
+    unsigned int id;
     std::string name;
     unsigned int score;
     bool isAI;
+    static unsigned int lastId;
 };
 
 
