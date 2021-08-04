0deb029e83b1b29103f9b22362bbb23d50273788
Amin_Seifi <aminseiifi@gmail.com>
2021-08-04 21:24:17 +0430

    Test game

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include "Model/Game.h"
 
 using namespace std;
 
 int main() {
+    // overload comparison operators for player obj (score)
+
+    Player p1;
+    cin >> p1;
+    Player p2;
+    cin >> p2;
+
+    Board b;
+
+    Game game(p1, p2, b);
+    while(!game.isEnd())
+    {
+        game.playerTurn();
+    }
+    cout << game.getWinner().getName() << " is win!" << endl;
 
     return 0;
 }
