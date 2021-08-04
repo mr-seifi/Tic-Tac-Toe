a5529f3332058b8546ab72acfc999c6ec3f02821
Amin_Seifi <aminseiifi@gmail.com>
2021-08-04 18:34:27 +0430

    Define data members of game class

--- Model/Game.h
+++ Model/Game.h
@@ -1,9 +1,20 @@
 #ifndef TIC_TAC_TOE_GAME_H
 #define TIC_TAC_TOE_GAME_H
 
+#include "Player.h"
+#include "Board.h"
+#include <utility>
 
 class Game {
+public:
+    Game();
+    Game(Player, Player, Board);
+    const Player &whoTurn();
+    void turn();
 
+private:
+    std::pair<Player, Player> players;
+    Board board;
 };
 
 
