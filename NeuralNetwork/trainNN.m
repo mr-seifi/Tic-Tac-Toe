51208f7564ea7db232a5ed5e1de823cb6feb76fe
Amin_Seifi <aminseiifi@gmail.com>
2021-08-21 19:54:18 +0430

    Change weights

--- NeuralNetwork/trainNN.m
+++ NeuralNetwork/trainNN.m
@@ -16,10 +16,8 @@ Theta3 = randInitializeWeights(second_hidden_layer_size, num_labels);
 nn_params = [Theta1(:); Theta2(:); Theta3(:)];
 
 % Create "short hand" for the cost function to be minimized
-costFunction = @(t) linearRegCostFunction(X, y, t, lambda);
-lambda = 1e-3;
 costFunction = @(nn_params) nnCostFunction(nn_params, input_layer_size, first_hidden_layer_size, second_hidden_layer_size, num_labels, X, y, lambda);
-options = optimset('MaxIter', 400);
+options = optimset('MaxIter', 300);
 [nn_params1, cost] = fmincg(costFunction, nn_params, options);
 Theta1 = reshape(nn_params1(1:first_hidden_layer_size * (input_layer_size + 1)), first_hidden_layer_size, (input_layer_size + 1));
 Theta2 = reshape(nn_params1((1 + (first_hidden_layer_size * (input_layer_size + 1))):(((first_hidden_layer_size * (input_layer_size + 1))) + (second_hidden_layer_size * (first_hidden_layer_size + 1)))), second_hidden_layer_size, (first_hidden_layer_size + 1));
